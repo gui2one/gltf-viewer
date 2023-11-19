5a4bcbd8c367e4bdcb9d5257f7080b52a71265b2
gui2one <guillaume.rouault.fx@gmail.com>
2023-11-19 17:12:19 +0100

    better deploy, added "confirmDelete"

--- deploy/deployV2.mjs
+++ deploy/deployV2.mjs
@@ -1,12 +1,38 @@
 import * as ftp from "basic-ftp";
 import env from "dotenv";
-
+import readline from "readline"
 env.config();
 
 const LOCAL_DIR = "./dist";
 const DISTANT_DIR = "/httpdocs/gui2one/gltf-viewer";
 
-example();
+let do_delete = await confirmDelete(`About to delete content from ${DISTANT_DIR}\n are you sure ? (y/n) : `);
+
+if( do_delete){
+
+    example();
+}
+
+async function confirmDelete(msg)
+{
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });    
+    return new Promise((resolve)=>{
+        rl.question(`\x1b[33m${msg}\x1b[0m`, ans=>{
+            rl.close();
+            if( ans !== "y") {
+                console.warn("aborting ... ")
+                resolve(false);
+            }else{
+
+                console.warn("removing distant directory content")
+                resolve(true);
+            }
+        })
+    });
+}
 async function example() {
     const client = new ftp.Client()
     client.ftp.verbose = true
